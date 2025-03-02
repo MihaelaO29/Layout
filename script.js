a964dc99687ee86ba91f2758aa2f632249e2c88b
MihaelaO29 <opriamihaela22@yahoo.com>
2025-03-02 23:04:21 +0200

    added description section

--- script.js
+++ script.js
@@ -1,29 +1,17 @@
-document.getElementById("flag-selector").addEventListener("click", function () {
+document.getElementById("flag-selector").addEventListener("click", () => {
     document.querySelector(".country-list").style.display = "block";
 });
 
-document.querySelectorAll(".country-list li").forEach(function (item) {
-    item.addEventListener("click", function () {
-        let flagUrl = this.getAttribute("data-flag");
-        let countryCode = this.getAttribute("data-code");
-
-
-        document.getElementById("selected-flag").src = flagUrl;
-        document.getElementById("selected-code").textContent = countryCode;
-
-
-        let phoneInput = document.getElementById("phone");
-        if (!phoneInput.value.startsWith(countryCode)) {
-            phoneInput.value = countryCode + " ";
-        }
-
-
+document.querySelectorAll(".country-list li").forEach(item => {
+    item.addEventListener("click", () => {
+        document.getElementById("selected-flag").src = item.getAttribute("data-flag");
+        document.getElementById("selected-code").textContent = item.getAttribute("data-code");
+        document.getElementById("phone").value = document.getElementById("phone").value.replace(/^\+\d+\s*/, '');
         document.querySelector(".country-list").style.display = "none";
     });
 });
 
-document.getElementById('phone').addEventListener('input', function(event) {
-    var inputValue = event.target.value;
-        var cleanedValue = inputValue.replace(/[^0-9]/g, '');
-        event.target.value = cleanedValue;
+
+document.getElementById('phone').addEventListener('input', (event) => {
+    event.target.value = event.target.value.replace(/\D/g, '');
 });
